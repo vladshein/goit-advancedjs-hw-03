4d93361b0d7063022cfe901e7cdea16c24679e53
vladshein <vlshein1@gmail.com>
2025-03-16 23:54:09 +0200

    add solution to HW3

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,92 +1,24 @@
 // Описаний в документації
 import iziToast from "izitoast";
 
-let selectedDate = "";
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "49371297-53cf19ada26e8c05638a83539";
 
-const timer = {
-  intervalId: null,
-  refs: {
-    days: document.querySelector("[data-days]"),
-    hours: document.querySelector("[data-hours]"),
-    minutes: document.querySelector("[data-minutes]"),
-    seconds: document.querySelector("[data-seconds]"),
-    inputArea: document.querySelector("input#datetime-picker"),
-  },
-
-  start() {
-    this.intervalId = setInterval(() => {
-      const diff = selectedDate - Date.now();
-
-      if (diff <= 0) {
-        this.stop();
-
-        return;
-      }
-
-      const timeComponents = this.getTimeComponents(diff);
-
-      this.refs.days.textContent = this.pad(timeComponents.days);
-      this.refs.hours.textContent = this.pad(timeComponents.hours);
-      this.refs.minutes.textContent = this.pad(timeComponents.minutes);
-      this.refs.seconds.textContent = this.pad(timeComponents.seconds);
-
-      btn.disabled = true;
-      this.refs.inputArea.disabled = true;
-    }, 1000);
-  },
-
-  stop() {
-    clearInterval(this.intervalId);
-    btn.disabled = false;
-    this.refs.inputArea.disabled = false;
-  },
-
-  getTimeComponents(diff) {
-    const days = Math.floor(diff / 1000 / 60 / 60 / 24);
-    const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
-    const minutes = Math.floor(diff / 1000 / 60) % 60;
-    const seconds = Math.floor(diff / 1000) % 60;
-
-    return {
-      days,
-      hours,
-      minutes,
-      seconds,
-    };
-  },
-
-  pad(value) {
-    return String(value).padStart(2, "0");
-  },
-};
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-
-  onClose(selectedDates) {
-    selectedDate = selectedDates[0];
-
-    const diff = selectedDate - Date.now();
-    console.log("Selected date:", selectedDate);
-    if (diff <= 0) {
-      iziToast.show({
-        title: "",
-        message: "Please choose a date in the future",
-      });
-      // alert("Please choose a date in the future");
-      btn.disabled = true;
-      return;
+function getPhotos(query) {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+  });
+  return fetch(`${BASE_URL}?${params}`).then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
     }
-    btn.disabled = false;
-  },
-};
 
-flatpickr("input#datetime-picker", options);
+    return res.json();
+  });
+}
 
-const btn = document.querySelector("button");
-btn.addEventListener("click", event => {
-  timer.start();
-});
+export { getPhotos };
--- src/js/render-functions.js.js
+++ src/js/render-functions.js.js
@@ -1,47 +1,50 @@
 import iziToast from "izitoast";
 
-const form = document.querySelector(".form");
+// create a template
+function processImage({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  const liTemplate = `
+  <div class="gallery-item">
+    <li>
+      <a class="gallery-link" href="${largeImageURL}">
+        <img
+          class="gallery-image"
+          src="${webformatURL}"
+          alt="${tags}"
+        />
+      </a>
+    </li>
+    <ul class="gallery-ul">
+      <li class="gallery-li">
+        <p>Likes</p>
+        <p>${likes}</p>
+      </li>
+      <li class="gallery-li">
+        <p>Views</p>
+        <p>${views}</p>
+      </li>
+      <li class="gallery-li">
+        <p>Comments</p>
+        <p>${comments}</p>
+      </li>
+      <li class="gallery-li">
+        <p>Downloads</p>
+        <p>${downloads}</p>
+      </li>
+    </ul>
+  </div>`;
+  return liTemplate;
+}
 
-// let formDelay = null;
-// let radioValue = null;
+function updateGallery(photos) {
+  return photos.map(processImage).join("");
+}
 
-form.addEventListener("submit", event => {
-  event.preventDefault();
-  console.dir(event.target);
-  console.log("send form!");
-
-  let formDelay = form.querySelector("input[name='delay']").value;
-  let radioValue = form.querySelector('input[name="state"]:checked').value;
-
-  console.log(formDelay);
-  console.log(radioValue);
-
-  form.reset();
-
-  console.log(formDelay);
-  console.log(radioValue);
-
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (radioValue === "fulfilled") {
-        resolve(formDelay);
-      } else {
-        reject(formDelay);
-      }
-    }, formDelay);
-  });
-
-  promise
-    .then(result => {
-      iziToast.show({
-        title: "",
-        message: `Fulfilled promise in ${result}ms`,
-      });
-    })
-    .catch(error => {
-      iziToast.show({
-        title: "",
-        message: `Rejected promise in ${error}ms`,
-      });
-    });
-});
+export { updateGallery };
